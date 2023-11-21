53e0c50ff848deb4f93612b395e991880a8416f2
anhtu4156 <anhtu4156@gmail.com>
2023-11-22 01:13:02 +0700

    giao diện danh mục

--- admin/content.php
+++ admin/content.php
@@ -1,6 +1,5 @@
 <?php
-include "leftbar.php";
-include "topbar.php";
+
 ?>
         
             
--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,16 @@
 <?php
 
 include "header.php";
+include "leftbar.php";
+include "topbar.php";
 
 
 if (isset($_GET['act']) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
-        case "":
-
+        // danh mục dịch vụ
+        case "danhmuc_dv":
+            include "module/danhmuc/list.php";
             break;
 
     }
