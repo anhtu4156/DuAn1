f099b68a62addd1963cbceb86d8f386f97497768
anhtu4156 <anhtu4156@gmail.com>
2023-11-25 00:36:14 +0700

    Revert "loi ket trang"
    This reverts commit ba7a76f9877def80346d06596bfa1b5cd0195c9f.

--- admin/module/dich_vu/list.php
+++ admin/module/dich_vu/list.php
@@ -35,47 +35,45 @@
                     <div class="w-full relative mb-4">
                         <div class="flex-auto p-0 md:p-4">
 
+                            <form action="#" method="post">
+                                <div class="flex flex-wrap gap-4 mb-3">
+                                    <div class="mb-2 w-44 flex" >
+                                        <select name="iddm" id="Category" class="w-full inline-block rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500  dark:hover:border-slate-700">
+                                            <option value="0" class="dark:text-slate-700">Tất cả danh mục</option>
+                                            <?php
+                                            foreach ($listdanhmuc_dv as $danhmuc) {
+                                                extract($danhmuc);
+                                                echo '<option value="' . $id . '" class="dark:text-slate-700">' . $ten_loai_dv . '</option>';
+                                            }
+                                            ?>
 
-                            <div class="flex flex-wrap gap-4 mb-3">
-                                <div class="mb-2 w-44 flex">
-                                    <select name="iddm" id="Category" class="w-full inline-block rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500  dark:hover:border-slate-700">
-                                        <option value="0" class="dark:text-slate-700">Tất cả danh mục</option>
-                                        <?php
-                                        foreach ($listdanhmuc_dv as $danhmuc) {
-                                            extract($danhmuc);
-                                            echo '<option value="' . $id . '" class="dark:text-slate-700">' . $ten_loai_dv . '</option>';
-                                        }
-                                        ?>
-
-                                    </select>
-                                    <input class="ms-3 inline-block focus:outline-none bg-brand-500 mt-1 text-white hover:bg-brand-600 hover:text-white  text-md font-medium py-2 px-4 rounded" type="submit" name="clickOK" value="GO">
-                                </div>
-                                <div class="mb-2 w-36">
-                                    <!-- <select id="Vendor" class="w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500  dark:hover:border-slate-700">
+                                        </select>
+                                        <input class="ms-3 inline-block focus:outline-none bg-brand-500 mt-1 text-white hover:bg-brand-600 hover:text-white  text-md font-medium py-2 px-4 rounded" type="submit" name="clickOK" value="GO">
+                                    </div>
+                                    <div class="mb-2 w-36">
+                                        <!-- <select id="Vendor" class="w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500  dark:hover:border-slate-700">
                                         <option class="dark:text-slate-700">Vendor</option>
                                         <option class="dark:text-slate-700">Vendor-2</option>
                                         <option class="dark:text-slate-700">Vendor-3</option>
                                     </select> -->
-                                </div>
-                                <div class="ms-auto">
-                                    <form>
+                                    </div>
+                                    <div class="ms-auto">
                                         <div class="relative">
                                             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                                 <i data-lucide="search" class="z-[1] w-5 h-5 stroke-slate-400"></i>
                                             </div>
                                             <input type="search" id="productSearch" class="form-input w-52 rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500  dark:hover:border-slate-700 pl-10 p-2.5" placeholder="search">
                                         </div>
-                                    </form>
+                                    </div>
+                                    <div>
+                                        <a href="index.php?act=them_dv">
+                                            <button class="inline-block focus:outline-none bg-brand-500 mt-1 text-white hover:bg-brand-600 hover:text-white  text-md font-medium py-2 px-4 rounded">
+                                                Thêm dịch vụ
+                                            </button>
+                                        </a>
+                                    </div>
                                 </div>
-                                <div>
-                                    <a href="index.php?act=them_dv">
-                                        <button class="inline-block focus:outline-none bg-brand-500 mt-1 text-white hover:bg-brand-600 hover:text-white  text-md font-medium py-2 px-4 rounded">
-                                            Thêm dịch vụ
-                                        </button>
-                                    </a>
-                                </div>
-                                
-                            </div>
+                            </form>
 
 
                             <div id="myTabContent">
